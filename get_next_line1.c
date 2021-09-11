4510c5460d76cbf0aa8d6c066105ae7fc7793cdf
Fernanda Eschallots <feschall@oa-h3.msk.21-school.ru>
2021-09-11 11:43:42 +0300

    new ed.

--- get_next_line1.c
+++ get_next_line1.c
@@ -1,32 +1,34 @@
-#include "get_next_line.h"
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
 
-int get_nex_line(char **line)
+int	get_next_line(char **line)
 {
-    int i;
-    int l;
-    int r;
-    char c;
-    char *tmp;
+	int		i;
+	int		l;
+	int		r;
+	char	c;
+	char	*tmp;
 
-    r = 0;
-    l = 1;
-    if (!(*line = malloc(l)))
-        return (-1);
-    (*line)[0] = 0;
-    while ((r = read(0, &c, 1)) && l++ && c != '\n')
-    {
-        if (!(tmp = malloc(l)))
-        {
-            free(*line);
-            return (-1);
-        }
-        i = -1;
-        while (++i < l - 2)
-            tmp[i] = (*line)[i];
-        tmp[i] = c;
-        tmp[i + 1] = 0;
-        free(*line);
-        *line = tmp;     
-    }
-    return (r);
+	r = 0;
+	l = 1;
+	if (!(*line = malloc(l)))
+		return (-1);
+	(*line)[0] = 0;
+	while ((r = read(0, &c, 1)) && l++ && c != '\n')
+	{
+		if (!(tmp = malloc(l)))
+		{
+			free(*line);
+			return (-1);
+		}
+		i = -1;
+		while (++i < l - 2)
+			tmp[i] = (*line)[i];
+		tmp[i] = c;
+		tmp[i + 1] = 0;
+		free(*line);
+		*line = tmp;     
+	}
+	return (r);
 }
