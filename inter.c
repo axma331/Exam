5e0b7b60622fad932f30e13a1adf4cca89896d36
Fernanda Eschallots <feschall@oa-h3.msk.21-school.ru>
2021-09-16 10:30:52 +0300

    new i\u\g

--- inter.c
+++ inter.c
@@ -27,38 +27,3 @@ int main(int ac, char **av)
 	write(1, "\n", 1);
 	return (0);
 }
-
-// int  main(int ac, char **av)
-// {
-// 	int i, j, c;
-
-// 	if (ac == 3)
-// 	{
-// 		i = 0;
-// 		while (av[1][i])
-// 		{
-// 			j = 0;
-// 			while (av[2][j])
-// 			{
-// 				if (av[1][i] == av[2][j])
-// 				{
-// 					c = 0;
-// 					while (c < i)
-// 					{
-// 						if (av[1][i] == av[1][c])
-// 							break ;
-// 						c++;
-// 					}
-// 					if (i != c && av[1][i] == av[1][c])
-// 							break ;
-// 					write (1, &av[1][i], 1);
-// 					break ;
-// 				}
-// 				j++;
-// 			}
-// 			i++;
-// 		}
-// 	}
-// 	write(1, "\n", 1);
-// 	return (0);
-// }
