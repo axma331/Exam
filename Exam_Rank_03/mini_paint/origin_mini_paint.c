bd63efd0ebcd5bc224cc46ee31694443d85cb5a4
Fernanda Eschallots <feschall@oa-h3.msk.21-school.ru>
2021-09-19 13:22:58 +0300

    edit mini_paint

--- Exam_Rank_03/mini_paint/origin_mini_paint.c
+++ Exam_Rank_03/mini_paint/origin_mini_paint.c
@@ -3,13 +3,15 @@
 #include <stdlib.h>
 #include <math.h>
 
-typedef struct	s_zone {
+typedef struct	s_zone
+{
 	int		width;
 	int		height;
 	char	background;
 }				t_zone;
 
-typedef struct	s_shape {
+typedef struct	s_shape
+{
 	char	type;
 	float	x;
 	float	y;
@@ -17,30 +19,38 @@ typedef struct	s_shape {
 	char	color;
 }				t_shape;
 
-int	ft_strlen(char const *str) {
-	int	i = 0;
+int
+	ft_strlen(char const *str)
+{
+	int	i;
 
+	i = 0;
 	while (str[i])
 		i++;
 	return (i);
 }
 
-char	*get_zone(FILE *file, t_zone *zone) {
-	int		i = 0;
+char
+	*get_zone(FILE *file, t_zone *zone)
+{
+	int		i;
 	char	*tmp;
 
 	if (fscanf(file, "%d %d %c\n", &zone->width, &zone->height, &zone->background) != 3)
 		return (NULL);
-	if (zone->width <= 0 || 300 < zone->width || zone->height <= 0 || 300 < zone->height)
+	if (zone->width <= 0 || zone->width > 300 || zone->height <= 0 || zone->height > 300)
 		return (NULL);
 	if (!(tmp = (char*)malloc(sizeof(*tmp) * (zone->width * zone->height))))
 		return (NULL);
+	i = 0;
 	while (i < zone->width * zone->height)
 		tmp[i++] = zone->background;
 	return (tmp);
 }
 
-int	in_circle(float x, float y, t_shape *shape) {
+int
+	in_circle(float x, float y, t_shape *shape)
+{
 	float	distance;
 
 	distance = sqrtf(powf(x - shape->x, 2.) + powf(y - shape->y, 2.));
@@ -53,22 +63,37 @@ int	in_circle(float x, float y, t_shape *shape) {
 	return (0);
 }
 
-void	draw_shape(t_zone *zone, char *drawing, t_shape *shape) {
-	int	x, y, is_it;
+void
+	draw_shape(t_zone *zone, char *drawing, t_shape *shape)
+{
+	int	y;
+	int	x;
+	int	is_it;
 
-	for (y = 0; y < zone->height; y++)
-		for (x = 0; x < zone->width; x++) {
+	y = 0;
+	while (y < zone->height)
+	{
+		x = 0;
+		while (x < zone->width)
+		{
 			is_it = in_circle((float)x, (float)y, shape);
-			if ((shape->type == 'c' && is_it == 2) || (shape->type == 'C' && is_it))
+			if ((shape->type == 'c' && is_it == 2)
+				|| (shape->type == 'C' && is_it))
 				drawing[(y * zone->width) + x] = shape->color;
+			x++;
 		}
+		y++;
+	}
 }
 
-int	draw_shapes(FILE *file, t_zone *zone, char *drawing) {
+int
+	draw_shapes(FILE *file, t_zone *zone, char *drawing)
+{
 	t_shape	tmp;
 	int		ret;
 
-	while ((ret = fscanf(file, "%c %f %f %f %c\n", &tmp.type, &tmp.x, &tmp.y, &tmp.radius, &tmp.color)) == 5) {
+	while ((ret = fscanf(file, "%c %f %f %f %c\n", &tmp.type, &tmp.x, &tmp.y, &tmp.radius, &tmp.color)) == 5)
+	{
 		if (tmp.radius <= 0.00000000 || (tmp.type != 'c' && tmp.type != 'C'))
 			return (0);
 		draw_shape(zone, drawing, &tmp);
@@ -78,20 +103,31 @@ int	draw_shapes(FILE *file, t_zone *zone, char *drawing) {
 	return (1);
 }
 
-void	draw_drawing(t_zone *zone, char *drawing) {
-	for (int i = 0; i < zone->height; i++) {
+void
+	draw_drawing(t_zone *zone, char *drawing)
+{
+	int	i;
+
+	i = 0;
+	while (i < zone->height)
+	{
 		write(1, drawing + (i * zone->width), zone->width);
 		write(1, "\n", 1);
+		i++;
 	}
 }
 
-int	str_error(char const *str) {
+int
+	str_error(char const *str)
+{
 	if (str)
 		write(1, str, ft_strlen(str));
 	return (1);
 }
 
-int	clear_all(FILE *file, char *drawing, char const *str) {
+int
+	clear_all(FILE *file, char *drawing, char const *str)
+{
 	if (file)
 		fclose(file);
 	if (drawing)
@@ -101,7 +137,9 @@ int	clear_all(FILE *file, char *drawing, char const *str) {
 	return (1);
 }
 
-int main(int argc, char **argv) {
+int
+	main(int argc, char **argv)
+{
 	FILE	*file;
 	t_zone	zone;
 	char	*drawing;
