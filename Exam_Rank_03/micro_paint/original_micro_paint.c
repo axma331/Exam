9096f8d276728ffbd80e7066be65388fec73a0ec
Fernanda Eschallots <feschall@oa-h3.msk.21-school.ru>
2021-09-19 13:09:36 +0300

    edit micro_paint & add Exam_Pank_04

--- Exam_Rank_03/micro_paint/original_micro_paint.c
+++ Exam_Rank_03/micro_paint/original_micro_paint.c
@@ -2,13 +2,15 @@
 #include <unistd.h>
 #include <stdlib.h>
 
-typedef struct	s_zone {
+typedef struct	s_zone
+{
 	int		width;
 	int		height;
 	char	background;
 } 				t_zone;
 
-typedef struct	s_shape {
+typedef struct	s_shape
+{
 	char	type;
 	float	x;
 	float	y;
@@ -18,57 +20,68 @@ typedef struct	s_shape {
 	struct s_shape	*next;
 }				t_shape;
 
-int	ft_strlen(char const *s) {
+int	ft_strlen(char const *str)
+{
 	int	i = 0;
-	while (s && s[i])
+	while (str[i])
 		i++;
 	return (i);
 }
 
-int	str_error(char const *s, int ret) {
-	write(1, s, ft_strlen(s));
+int	str_error(char const *str, int ret)
+{
+	write(1, str, ft_strlen(str));
 	return (ret);
 }
 
-int	clear_all(FILE *file, char *drawing) {
+int	clear_all(FILE *file, char *drawing)
+{
 	fclose(file);
 	if (drawing)
 		free(drawing);
 	return (1);
 }
 
-int	check_zone(t_zone *zone) {
-	return (0 < zone->width && zone->width <= 300
-			&& 0 < zone->height && zone->height <= 300);
+int	check_zone(t_zone *zone)
+{
+	return (zone->width > 0 && zone->width <= 300
+			&& zone->height > 0 && zone->height <= 300);
 }
 
-int	check_shape(t_shape *shape) {
-	return (0.00000000 < shape->width && 0.00000000 < shape->height
+int	check_shape(t_shape *shape)
+{
+	return (shape->width > 0.00000000 && shape->height > 0.00000000
 			&& (shape->type == 'r' || shape->type == 'R'));
 }
 
-int	get_zone(FILE *file, t_zone *zone) {
-	int ret;
+int	get_zone(FILE *file, t_zone *zone)
+{
+	int scan_ret;
 
-	if ((ret = fscanf(file, "%d %d %c\n", &zone->width, &zone->height, &zone->background)) != 3)
+	if ((scan_ret = fscanf(file, "%d %d %c\n", &zone->width, &zone->height, &zone->background)) != 3)
 		return (0);
-	if (!check_zone(zone) || ret == -1)
+	if (!check_zone(zone))
+		return (0);
+	if (scan_ret == -1)
 		return (0);
 	return (1);
 }
 
-char	*paint_background(t_zone *zone) {
+char	*paint_background(t_zone *zone)
+{
 	char	*drawing;
-	int		i = 0;
+	int		i;
 
 	if (!(drawing = (char*)malloc(sizeof(*drawing) * (zone->width * zone->height))))
 		return (NULL);
+	i = 0;
 	while (i < zone->width * zone->height)
 		drawing[i++] = zone->background;
 	return (drawing);
 }
 
-int	in_rectangle(float x, float y, t_shape *shape) {
+int	in_rectangle(float x, float y, t_shape *shape)
+{
 	if (((x < shape->x || (shape->x + shape->width < x))
 		|| (y < shape->y)) || (shape->y + shape->height < y))
 		return (0);
@@ -80,38 +93,57 @@ int	in_rectangle(float x, float y, t_shape *shape) {
 
 void	draw_shape(char **drawing, t_shape *shape, t_zone *zone)
 {
-	int	i, j, ret;
+	int	i;
+	int	j;
+	int	ret;
 
-	for (i = 0; i < zone->height; i++)
-		for (j = 0; j < zone->width; j++) {
+	i = 0;
+	while (i < zone->height)
+	{
+		j = 0;
+		while (j< zone->width)
+		{
 			ret = in_rectangle(j, i, shape);
-			if ((shape->type == 'r' && ret == 2) || (shape->type == 'R' && ret))
+			if ((shape->type == 'r' && ret == 2)
+				|| (shape->type == 'R' && ret))
 				(*drawing)[(i * zone->width) + j] = shape->color;
+			j++;
 		}
+		i++;
+	}
 }
 
-int	draw_shapes(FILE *file, char **drawing, t_zone *zone) {
+int	draw_shapes(FILE *file, char **drawing, t_zone *zone)
+{
 	t_shape	tmp;
-	int		ret;
+	int		scan_ret;
 
-	while ((ret = fscanf(file, "%c %f %f %f %f %c\n", &tmp.type, &tmp.x, &tmp.y, &tmp.width, &tmp.height, &tmp.color)) == 6) {
+	while ((scan_ret = fscanf(file, "%c %f %f %f %f %c\n", &tmp.type, &tmp.x, &tmp.y, &tmp.width, &tmp.height, &tmp.color)) == 6)
+	{
 		if (!check_shape(&tmp))
 			return (0);
 		draw_shape(drawing, &tmp, zone);
 	}
-	if (ret != -1)
+	if (scan_ret != -1)
 		return (0);
 	return (1);
 }
 
-void	draw_drawing(char *drawing, t_zone *zone) {
-	for (int i = 0; i < zone->height; i++) {
+void	draw_drawing(char *drawing, t_zone *zone)
+{
+	int	i;
+
+	i = 0;
+	while (i < zone->height)
+	{
 		write(1, drawing + (i * zone->width), zone->width);
 		write(1, "\n", 1);
+		i++;
 	}
 }
 
-int	main(int argc, char **argv) {
+int	main(int argc, char **argv)
+{
 	t_zone	zone;
 	FILE	*file;
 	char	*drawing;
